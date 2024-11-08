51ce153eea09e2c679f9dc4b59c3b85af9e0c6e1
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-11-08 18:26:12 +0530

    demo navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { NavigationMenuDemo } from "@/components/navigation"
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen dotted-background bg-background max-w-5xl mx-auto">
       <NavigationMenuDemo />
     </main>
   );
